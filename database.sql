ed63e7e44df244eebde1b4a2757bdc11bfedf5b0
Evan Langer <elanger@live.com>
2023-12-04 18:46:44 -0600

    sourced sever image files

--- database.sql
+++ database.sql
@@ -9,5 +9,11 @@ CREATE TABLE "gallery" (
 INSERT INTO "gallery" 
   ("url", "title", "description")
   VALUES
-  ('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.');
+  ('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.')
+  ('images/Gaia.webp', 'Mother Earth', 'An interpretation of the goddess of the Earth.')
+  ('images/theGoodtheBadandtheJolly.jpg', 'Santas got a list!', 'Santas back and hes got a present.')
+  ('images/thescreammunchnye.jpg.webp', 'Surprise party!', 'A classic reimagined.')
+  ('images/Trex.png', 'T Rex!', 'The once and forever king.')
+  ('images/unicorn.webp', 'Magesty caged', 'Domestication of our imagination.')
+  ('images/vasnetsov_samolet.jpg.webp', 'Climb aboard!', 'Discovery on a magic carpet ride.');
   
